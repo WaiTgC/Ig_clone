f99ab7bb6685d38c342314106a2a9fd11f88a2e0
WaiTgC <waiyanlintg@gmail.com>
2025-05-31 02:39:15 +0700

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return <Text>Hello World</Text>;
+}
---
+++ src/app/about.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.subtitle}>about us page</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: "#fff",
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
